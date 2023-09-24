83f72a1644d491249481d9899aebcc93f046cce1
Aryan Raj <aryanraj.raushan@gmail.com>
2023-09-24 21:06:34 +0530

    add folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Order from './pages/order/Order';
+import Cart from './pages/cart/cart';
+import Home from './pages/home/Home';
+import Dashboard from './pages/admin/dashboard/dashboard';
+import NoPage from './pages/no-page/NoPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      app hello
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/order" element={<Order/>} />
+      <Route path="/cart" element={<Cart/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/*" element={<NoPage/>} />
+    </Routes>
+  </Router>
   );
 }
 
