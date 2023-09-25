514cb7676e38a0a9ec72eb960509028f5dd0ea42
Aryan Raj <aryanraj.raushan@gmail.com>
2023-09-25 11:02:20 +0530

    folder restructure navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -10,19 +9,22 @@ import Cart from './pages/cart/cart';
 import Home from './pages/home/Home';
 import Dashboard from './pages/admin/dashboard/dashboard';
 import NoPage from './pages/no-page/NoPage';
+import MyState from "./context/myState";
 
 const App = () => {
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/order" element={<Order/>} />
-      <Route path="/cart" element={<Cart/>} />
-      <Route path="/dashboard" element={<Dashboard/>} />
-      <Route path="/*" element={<NoPage/>} />
-    </Routes>
-  </Router>
+    <MyState>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+      </Router>
+    </MyState>
   );
-}
+};
 
 export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Layout from '../../components/layout/layout'
+import { useContext } from "react";
+import Layout from "../../components/layout/layout";
+import myContext from "../../context/myContext";
 
 const Home = () => {
+  const context = useContext(myContext);
+  console.log(context); // {name: 'Kamal Nayan', class: '9 C'}
+  // Destructure
+  // const { state } = context;
+  // console.log(state); // Kamal Nayan
   return (
-    <Layout>Home</Layout>
-  )
-}
-
-export default Home
+    <Layout>
+      {/* <h1>Name : {state.name}</h1> */}
+      Home
+    </Layout>
+  );
+};
+export default Home;
